6b0323d43e8fe254e9538c314847decd3facaff1
MH Parvez <mh.parvez.dev1@gmail.com>
2024-10-02 23:29:27 +0600

    Update 05-destructuring-parameter.js

--- Section-2/Chapter-03-Object/05-destructuring-parameter.js
+++ Section-2/Chapter-03-Object/05-destructuring-parameter.js
@@ -1,6 +1,4 @@
 
-// Example: 1
-
 var person = {
     name: "jaman",
     age: 20,
@@ -16,27 +14,3 @@ function printer({ name, age, address: { country, city } }) {
 }
 printer(person);
 
-
-// Example: 2
-
-var student = {
-    id: 2013030440,
-    name: "Tuhin Ahmed",
-    age: 23,
-    department: "EEE",
-    email: "tuhin453@gmail.com",
-};
-
-function reciveStudentData(student, emailSender) {
-
-    let { id, name, department, age, email } = student;
-
-    console.log(name, id, department, age);
-
-    age >= 18 ? emailSender(email) : console.log("not eligable");
-}
-
-reciveStudentData(student, (email) => {
-    
-    console.log(`Email Send Successfully`);
-});
