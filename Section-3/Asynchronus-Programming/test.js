d3e664fd96f12fb11dda04719b71b7e80c14b48c
MH Parvez <mh.parvez.dev1@gmail.com>
2024-12-01 23:50:26 +0600

    Update test.js

--- Section-3/Asynchronus-Programming/test.js
+++ Section-3/Asynchronus-Programming/test.js
@@ -1,5 +1,43 @@
 
 
+const kitchen = ["🍚", "🥦", "🥔", "🧅"];
+const friendName = "Hasan";
+
+const friedRice = (name) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (kitchen.includes("🍚") && kitchen.includes("🥦") && kitchen.includes("🥔") && kitchen.includes("🧅")) {
+                //Internet Data
+                resolve({
+                    status: true,
+                    value: `Hey ${name}, Ready to Eating Hot Fried Rice 🍲`
+                })
+            } else {
+                reject(new Error('Sorry bondu fartam nay ...'))
+            }
+        }, 2000)
+    });
+}
+
+friedRice(friendName)
+.then((resolveValue) => {
+    console.log(resolveValue)
+    resolveValue.value = 'Modifed Value'
+    return resolveValue
+}).then((resolveValueAgain) => {
+    console.log(resolveValueAgain)
+})
+
+.catch((error) => {
+    console.log(error)
+});
+
+const ReadyToGO = Promise.resolve("Ready to Goo..")
+
+ReadyToGO.then(data => {
+    console.log(data)
+})
+
 
 
 
